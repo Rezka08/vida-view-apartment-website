9f20fe1badcbff28a39405b89d6498e1b81bb957
andiaisar <andiaisar42@gmail.com>
2025-11-19 21:23:37 +0800

    feat: Add logo image to Navbar component

--- frontend/src/layouts/Navbar.jsx
+++ frontend/src/layouts/Navbar.jsx
@@ -49,11 +49,12 @@ const Navbar = () => {
         <div className="flex justify-between h-16">
           {/* Logo */}
           <div className="flex items-center">
-            <Link to="/" className="flex items-center space-x-2">
-              <div className="w-10 h-10 bg-purple-600 rounded-lg flex items-center justify-center">
-                <span className="text-white font-bold text-xl">VV</span>
-              </div>
-              <span className="text-xl font-bold text-gray-900">Vida View</span>
+            <Link to="/" className="flex items-center">
+              <img 
+                src="/img/logo.png" 
+                alt="Vida View Logo" 
+                className="h-28 w-auto"
+              />
             </Link>
           </div>
 
